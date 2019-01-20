5e4d1d43a605f5d577ea895f1dd906f5a613d9c8
KimJoon93 <jk1094@naver.com>
2019-01-20 12:38:38 +0900

    Update TeamB method

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
-    int getScoreTeamB = 0;
+    int scoreTeamB = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,5 +33,26 @@ public class MainActivity extends AppCompatActivity {
     public void addFreeThrowForTeamA(View view){
         scoreTeamA = scoreTeamA+1;
         displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeForTeamB(View view){
+        scoreTeamB = scoreTeamB+3;
+        displayForTeamB(scoreTeamB);
+    }
+    public void addTwoForTeamB(View view){
+        scoreTeamB = scoreTeamB+2;
+        displayForTeamA(scoreTeamB);
+    }
+    public void addFreeThrowForTeamB(View view){
+        scoreTeamB = scoreTeamB+1;
+        displayForTeamA(scoreTeamB);
     }
 }
