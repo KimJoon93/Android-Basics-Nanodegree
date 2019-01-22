453c9afc7c614e759dffbd1c9b5c62afc3ee4888
KimJoon93 <jk1094@naver.com>
2019-01-22 17:15:56 +0900

    Fix Error

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -28,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Total: $" +calculatePrice(quantity,10) + "\nThank you";
+        int price = calculatePrice(quantity,10);
+        String priceMessage = "Total: $" +price+ "\nThank you";
         displayMessage(priceMessage);
     }
 
