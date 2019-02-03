ac7b2a0ee0fd5006466c5a106854cf1f2d5a7dda
KimJoon93 <jk1094@naver.com>
2019-02-03 20:46:26 +0900

    Use ListView

--- Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -30,8 +31,8 @@ public class NumbersActivity extends AppCompatActivity {
 
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        ListView listView = (ListView) findViewById(R.id.list);
+        GridView gridView = (GridView) findViewById(R.id.list);
 
-        listView.setAdapter(itemsAdapter);
+        gridView.setAdapter(itemsAdapter);
     }
 }
