723766f613add22a297673f75e38feeed3ff1b31
KimJoon93 <jk1094@naver.com>
2019-02-07 23:18:44 +0900

    Playing state

--- Jtunes/app/src/main/java/com/example/android/jtunes/PlayingScreen.java
+++ Jtunes/app/src/main/java/com/example/android/jtunes/PlayingScreen.java
@@ -8,12 +8,13 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class PlayingScreen extends AppCompatActivity {
-
+    Boolean playingstate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playing_screen);
 
+        playingstate = true;
         TextView songTitle = (TextView)findViewById(R.id.playing_song_title_text);
         TextView singerName = (TextView) findViewById(R.id.playing_singer_name_text);
 
@@ -23,9 +24,18 @@ public class PlayingScreen extends AppCompatActivity {
         playbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                playbtn.setBackgroundResource(R.drawable.ic_play_button);
-                playingtext.setText("Not Playing");
-                playingtext.setTextColor(getResources().getColor(R.color.red));
+                if(playingstate==true){
+                    playbtn.setBackgroundResource(R.drawable.ic_play_button);
+                    playingtext.setText("Not Playing");
+                    playingtext.setTextColor(getResources().getColor(R.color.red));
+                    playingstate = false;
+                }else if(playingstate==false){
+                    playbtn.setBackgroundResource(R.drawable.ic_pause);
+                    playingtext.setText("Playing Now");
+                    playingtext.setTextColor(getResources().getColor(R.color.green));
+                    playingstate=true;
+                }
+
             }
         });
 
