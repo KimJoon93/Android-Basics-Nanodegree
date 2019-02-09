66585e6080ebfb2a6179506bd169cfac8d45a36e
KimJoon93 <jk1094@naver.com>
2019-02-09 23:54:08 +0900

    Ready for music App

--- MusicPlayer/app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playbtn = (Button)findViewById(R.id.playbtn);
+        Button pausebtn = (Button)findViewById(R.id.pausebtn);
+
+        playbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        pausebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
