c88554aa4fcd59bf924d46c7add8a2cdf7c230b3
KimJoon93 <jk1094@naver.com>
2019-02-10 14:50:01 +0900

    Study Lifecycle

--- Lifecycle/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lifecycle/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.lifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("TEST","onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("TEST","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("TEST","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("TEST","onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("TEST","onDestroy");
     }
 }
