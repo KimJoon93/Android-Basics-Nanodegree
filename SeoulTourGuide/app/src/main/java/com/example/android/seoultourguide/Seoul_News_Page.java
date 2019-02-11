d8e25c297b8b76f26621f42eef534b2c88e7e264
KimJoon93 <jk1094@naver.com>
2019-02-11 23:58:35 +0900

    ListAdapter

--- SeoulTourGuide/app/src/main/java/com/example/android/seoultourguide/Seoul_News_Page.java
+++ SeoulTourGuide/app/src/main/java/com/example/android/seoultourguide/Seoul_News_Page.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class Seoul_News_Page extends Fragment {
 
@@ -22,7 +23,13 @@ public class Seoul_News_Page extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_seoul__news__page, container, false);
+
+        ListView news_list = view.findViewById(R.id.news_listview);
+
+
+
         return view;
     }
 
