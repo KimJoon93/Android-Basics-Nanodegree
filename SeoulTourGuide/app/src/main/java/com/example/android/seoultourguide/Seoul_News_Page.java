2364367b0ce29d26e22a0aeab4c6d866e03f7743
KimJoon93 <jk1094@naver.com>
2019-02-11 19:13:25 +0900

    Ready for fragment

---
+++ SeoulTourGuide/app/src/main/java/com/example/android/seoultourguide/Seoul_News_Page.java
@@ -0,0 +1,29 @@
+package com.example.android.seoultourguide;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Seoul_News_Page extends Fragment {
+
+    public Seoul_News_Page() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_seoul__news__page, container, false);
+        return view;
+    }
+
+}
