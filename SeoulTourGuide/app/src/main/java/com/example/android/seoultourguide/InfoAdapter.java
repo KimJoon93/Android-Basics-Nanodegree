caed434af54a56fa9c0af0a3105f642e6cadfcb0
KimJoon93 <jk1094@naver.com>
2019-02-17 23:55:36 +0900

    Update Info Adapter

--- SeoulTourGuide/app/src/main/java/com/example/android/seoultourguide/InfoAdapter.java
+++ SeoulTourGuide/app/src/main/java/com/example/android/seoultourguide/InfoAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class InfoAdapter extends ArrayAdapter {
     public InfoAdapter(Context context, int resource) {
@@ -16,6 +17,9 @@ public class InfoAdapter extends ArrayAdapter {
         if(convertView == null){
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.info_layout,parent,false);
         }
+        TextView infoTitle = convertView.findViewById(R.id.info_title);
+        TextView infoContext = convertView.findViewById(R.id.info_context);
+        
 
         return convertView;
     }
